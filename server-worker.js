159742c101f9d8175026553ec078277233ff6525
Shunmathi007 <s03059525@gmail.com>
2025-07-11 11:13:26 +0530

    Add files via upload

---
+++ server-worker.js
@@ -0,0 +1,15 @@
+{
+  "name": "BioLoop",
+  "short_name": "BioLoop",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#e6f2e6",
+  "theme_color": "#008080",
+  "icons": [
+    {
+      "src": "logo.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
